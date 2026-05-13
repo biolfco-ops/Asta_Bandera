e23dd7eb265597440e6c4b4d43bc0e79488b0c37
Pancho Rdz <biolfco@gmail.com>
2026-05-13 13:30:27 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,335 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_arriba",
+      "name": "01_arriba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.9898635452925557,
+        "pitch": 0.8231283456326928,
+        "fov": 1.8738856864289415
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0372693050977286,
+          "pitch": 1.1833502231580617,
+          "rotation": 0,
+          "target": "1-02_plaza_abajo"
+        },
+        {
+          "yaw": 0.49332328681675186,
+          "pitch": 0.8327279766857281,
+          "rotation": 5.497787143782138,
+          "target": "3-04_asta_derecha"
+        },
+        {
+          "yaw": -2.4845336146826718,
+          "pitch": 0.8367144693075126,
+          "rotation": 7.0685834705770345,
+          "target": "4-05_asta_izquierda"
+        },
+        {
+          "yaw": 2.0393785129052624,
+          "pitch": 1.4579306604850384,
+          "rotation": 3.141592653589793,
+          "target": "2-03_asta_abajo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02_plaza_abajo",
+      "name": "02_plaza_abajo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.12537670341638218,
+        "pitch": 0.08937674690155362,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03929487790437136,
+          "pitch": 0.2460789135500665,
+          "rotation": 0,
+          "target": "2-03_asta_abajo"
+        },
+        {
+          "yaw": -1.6658419769548196,
+          "pitch": 0.19959564908386795,
+          "rotation": 3.141592653589793,
+          "target": "3-04_asta_derecha"
+        },
+        {
+          "yaw": 1.5240635523753294,
+          "pitch": 0.219199070595133,
+          "rotation": 3.141592653589793,
+          "target": "4-05_asta_izquierda"
+        },
+        {
+          "yaw": -0.05701732087036149,
+          "pitch": -1.1567792636040828,
+          "rotation": 0,
+          "target": "0-01_arriba"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03_asta_abajo",
+      "name": "03_asta_abajo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.950118244515334,
+        "pitch": 0.4847014255065414,
+        "fov": 1.8738856864289415
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0185682588947067,
+          "pitch": 0.17880887449896576,
+          "rotation": 3.141592653589793,
+          "target": "5-06_mar_centro"
+        },
+        {
+          "yaw": -0.5724283450224643,
+          "pitch": -1.5540057146166646,
+          "rotation": 0,
+          "target": "0-01_arriba"
+        },
+        {
+          "yaw": 1.044047934020469,
+          "pitch": 0.15230489325583463,
+          "rotation": 3.141592653589793,
+          "target": "3-04_asta_derecha"
+        },
+        {
+          "yaw": -1.4691669544926267,
+          "pitch": 0.16313795933933406,
+          "rotation": 3.141592653589793,
+          "target": "4-05_asta_izquierda"
+        },
+        {
+          "yaw": -0.08759199374545901,
+          "pitch": 0.4247176258323506,
+          "rotation": 0,
+          "target": "1-02_plaza_abajo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04_asta_derecha",
+      "name": "04_asta_derecha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.7927380019361756,
+        "pitch": -0.029047442743014074,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3246847943264477,
+          "pitch": -0.7265615181637894,
+          "rotation": 0,
+          "target": "0-01_arriba"
+        },
+        {
+          "yaw": -1.1711721194980171,
+          "pitch": 0.19703714897982394,
+          "rotation": 1.5707963267948966,
+          "target": "1-02_plaza_abajo"
+        },
+        {
+          "yaw": -2.287974661041609,
+          "pitch": -0.38142024073821545,
+          "rotation": 2.356194490192345,
+          "target": "5-06_mar_centro"
+        },
+        {
+          "yaw": -1.5096977106187897,
+          "pitch": 0.18478635595962167,
+          "rotation": 4.71238898038469,
+          "target": "2-03_asta_abajo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05_asta_izquierda",
+      "name": "05_asta_izquierda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.0737202086609052,
+        "pitch": -0.09551134641561987,
+        "fov": 1.8738856864289415
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5955104108648541,
+          "pitch": 0.0416150307134906,
+          "rotation": 0,
+          "target": "3-04_asta_derecha"
+        },
+        {
+          "yaw": -0.8345853258844276,
+          "pitch": -0.866787647426758,
+          "rotation": 0,
+          "target": "0-01_arriba"
+        },
+        {
+          "yaw": -1.451465553135506,
+          "pitch": 0.32573788659152925,
+          "rotation": 13.351768777756625,
+          "target": "1-02_plaza_abajo"
+        },
+        {
+          "yaw": -0.7276620392373552,
+          "pitch": 0.24222154853650935,
+          "rotation": 14.137166941154074,
+          "target": "2-03_asta_abajo"
+        },
+        {
+          "yaw": -0.06502428213034506,
+          "pitch": -0.383531752086812,
+          "rotation": 3.9269908169872414,
+          "target": "5-06_mar_centro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06_mar_centro",
+      "name": "06_mar_centro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.16949785527920724,
+        "pitch": 0.5956732665531934,
+        "fov": 1.8738856864289415
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03289342230568337,
+          "pitch": -0.0888664098763563,
+          "rotation": 3.141592653589793,
+          "target": "0-01_arriba"
+        },
+        {
+          "yaw": 0.06408664376866291,
+          "pitch": 0.9363082964877698,
+          "rotation": 3.141592653589793,
+          "target": "2-03_asta_abajo"
+        },
+        {
+          "yaw": 0.857150036500979,
+          "pitch": 0.6017493320923215,
+          "rotation": 4.71238898038469,
+          "target": "3-04_asta_derecha"
+        },
+        {
+          "yaw": -0.8640194652939535,
+          "pitch": 0.5888990366263549,
+          "rotation": 1.5707963267948966,
+          "target": "4-05_asta_izquierda"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ASTA BANDERA",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
